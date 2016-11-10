a1f3d1b80afcac4bac13bd6ba036a6c5bce0ca6d
Joydeep BHattacharjee <joydeepubuntu@gmail.com>
2016-11-10 09:41:39 +0000

    python mode

--- configure_vim.sh
+++ configure_vim.sh
@@ -11,4 +11,9 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+# install python-mode
+cd ~/.vim
+mkdir -p bundle && cd bundle
+git clone https://github.com/klen/python-mode.git
+
 vim +PluginInstall +qall
