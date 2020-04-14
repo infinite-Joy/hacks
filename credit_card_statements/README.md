7909f8d953a3bea15d5e2c274c853cf3b5e270e7
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2020-04-14 10:39:39 +0530

    read credit card statements.

---
+++ credit_card_statements/README.md
@@ -0,0 +1,10 @@
+# Understand cc statement
+
+To run this would need to pass in the following format
+
+```
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python read_credit_card_statement.py --filename <credit card pdf file> --password "mypassword"
+```
---
+++ credit_card_statements/read_credit_card_statement.py
@@ -0,0 +1,66 @@
+import tabula
+import pandas as pd
+import click
+
+#file = "560034_1002020003063579(1).pdf"
+
+def read_pdf_and_get_all_dataframes(filename, password):
+    """
+    read pdf and get all dataframes
+    """
+    # tables will have the list of all the dataframes
+    tables = tabula.read_pdf(filename, pages = "all", password=password)
+    
+    # we need to make a single dataframe
+    statement_df = pd.concat(tables)
+    return statement_df
+
+
+def get_amount(string):
+    """Get amount from the string.
+
+    Generally string is in the format '230.45 Dr'
+    """
+    for s in string.split():
+        try:
+            return float(s.replace(',',''))
+        except AttributeError:
+            pass
+
+
+def normalise_to_numbers(statement_df):
+    """Make dr to + and cr to -"""
+    statement_df['DR'] = statement_df['Amount (Rs.)'].apply(lambda x: get_amount(x) if 'Dr' in x else None)
+    statement_df['CR'] = statement_df['Amount (Rs.)'].apply(lambda x: get_amount(x) if 'Cr' in x else None)
+    statement_df = statement_df.fillna(0)
+    statement_df['final_amounts'] = statement_df.apply(lambda x: get_final_amounts(x.DR, x.CR), axis=1)
+    return statement_df
+
+
+def get_final_amounts(debit, credit):
+    return debit - credit
+
+
+def normalise_business_details(statement_df):
+    """clean up the transaction details to get the list of business where money spent"""
+    # this is because the details generally come in the below format
+    # DUNZO DIGITAL PRIVATE GURGAON IND - Ref No: MT200900083000010023679
+    statement_df['business'] = statement_df['Transaction Details'].apply(lambda x: x.split()[0] + " " + x.split()[1])
+    different_expenses = statement_df.groupby('business')['final_amounts'].sum()
+    return different_expenses
+
+
+@click.command()
+@click.option('--filename', help='pass the name of the pdf cc statement recieved from the bank')
+@click.option('--password', help='password to open the pdf file')
+def main(filename, password):
+    """SImple program to understand where i am spending my money."""
+    statement_df = read_pdf_and_get_all_dataframes(filename, password)
+    statement_df = normalise_to_numbers(statement_df)
+    expenses_df = normalise_business_details(statement_df)
+    print(expenses_df)
+
+
+if __name__ == "__main__":
+    main()
+
